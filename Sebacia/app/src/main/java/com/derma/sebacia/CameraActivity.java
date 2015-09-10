bcbad6511c14d6db901be571b04a0c250521a702
Nick Mathews <nick@codemettle.com>
2015-09-10 11:53:20 -0400

    Fixed Camera activity from crashing on load

--- Sebacia/app/src/main/java/com/derma/sebacia/CameraActivity.java
+++ Sebacia/app/src/main/java/com/derma/sebacia/CameraActivity.java
@@ -23,9 +23,11 @@ public class CameraActivity extends AppCompatActivity {
         mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        if(mCamera == null) {
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
     }
 
     public static Camera getCameraInstance(){
