cb2adf99cd06b427be2a4f80378ac147731812fd
Nick Mathews <nick@codemettle.com>
2015-09-15 12:37:20 -0400

    Added a button and saves pictures
    The camera activity now has a button to take pictures and saves the pictures to a private directory (accessible only to our app)

--- Sebacia/app/src/main/java/com/derma/sebacia/camera/CameraPreview.java
+++ Sebacia/app/src/main/java/com/derma/sebacia/camera/CameraPreview.java
@@ -61,7 +61,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         // set preview size and make any resize, rotate or
         // reformatting changes here
-        mCamera.setDisplayOrientation(0);
+        mCamera.setDisplayOrientation(90);
 
         // start preview with new settings
         try {
