603885ef10eded992474d7b6a2c4ad2628e8c04e
Nick Mathews <nick@codemettle.com>
2015-10-27 12:19:20 -0400

    starting work on showing a list of images in history

--- Sebacia/app/src/main/java/com/derma/sebacia/HistoryActivity.java
+++ Sebacia/app/src/main/java/com/derma/sebacia/HistoryActivity.java
@@ -1,5 +1,7 @@
 package com.derma.sebacia;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,10 +11,14 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.derma.sebacia.R;
 import com.derma.sebacia.data.Patient;
@@ -24,6 +30,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -32,8 +39,11 @@ public class HistoryActivity extends AppCompatActivity {
     
     databaseInterface db;
     
-    ImageView imageView;
-
+//    ImageView imageView;
+    ListView imageList;
+    
+    PictureAdapter adapter;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +51,14 @@ public class HistoryActivity extends AppCompatActivity {
 
         //TODO: pull images from database
 
-        imageView = (ImageView)this.findViewById(R.id.hist_view_img);
+//        imageView = (ImageView)this.findViewById(R.id.hist_view_img);
+        imageList = (ListView) findViewById(R.id.history_list);
+//        Picture[] emptyList = {};
+        ArrayList<Picture> emptyList = new ArrayList<>();
+        adapter = new PictureAdapter(getApplicationContext(), R.id.textView, emptyList);
+        imageList.setAdapter(adapter);
         
-        db = new LocalDb(getApplicationContext());
+        new loadDbTask().execute(getApplicationContext());
     }
     
     protected void onStart() {
@@ -62,17 +77,18 @@ public class HistoryActivity extends AppCompatActivity {
         
         protected void onPostExecute(List<Picture> pictures) {
             if(pictures.size() > 0) {
-                Picture topPic = pictures.get(0);
-                Bitmap bmp = topPic.getPicBitmap();
-
-                imageView.setImageBitmap(bmp);
-                imageView.setOnClickListener(new ImageView.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent i = new Intent(getApplicationContext(), FindDoctorActivity.class);
-                        startActivity(i);
-                    }
-                });
+//                Picture topPic = pictures.get(0);
+                adapter.addAll(pictures);
+//                Bitmap bmp = topPic.getPicBitmap();
+//
+//                imageView.setImageBitmap(bmp);
+//                imageView.setOnClickListener(new ImageView.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        Intent i = new Intent(getApplicationContext(), FindDoctorActivity.class);
+//                        startActivity(i);
+//                    }
+//                });
             }
         }
     }
@@ -97,5 +113,43 @@ public class HistoryActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class loadDbTask extends AsyncTask<Context, Void, Void> {
+
+        protected Void doInBackground(Context... contexts) {
+
+            db = new LocalDb(contexts[0]);
+
+            return null;
+        }
+
+    }
+    
+    private class PictureAdapter extends ArrayAdapter<Picture> {
+        
+        public PictureAdapter(Context context, int textViewResourceId, ArrayList<Picture> pictures) {
+            super(context, textViewResourceId, pictures);
+            this.addAll(pictures);
+        }
+        
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if(v == null) {
+                LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.activity_history, null);
+            }
+            
+            Picture pic = this.getItem(position);
+            if(pic != null) {
+                ImageView iv = (ImageView) findViewById(R.id.history_list_item_image);
+                if(iv != null) {
+                    iv.setImageBitmap(pic.getPicBitmap());
+                }
+            }
+            
+            return v;
+        }
     }
 }
